42acbaa199740dc49bd9651f15fcbc724934a328
iKenshu <undefined.hlo.o@gmail.com>
2015-05-21 07:17:27 -0430

    Agregando Github

--- login/login/settings.py
+++ login/login/settings.py
@@ -78,6 +78,7 @@ AUTHENTICATION_BACKENDS = (
     'social.backends.facebook.FacebookOAuth2',
     'social.backends.google.GoogleOAuth2',
     'social.backends.twitter.TwitterOAuth',
+    'social.backends.github.GithubOAuth2',
     'django.contrib.auth.backends.ModelBackend',
 )
 
